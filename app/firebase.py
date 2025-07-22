84407f70b09bd10a7bfe02048ee5ec69c760ed56
Kent Carl A. Banzon <kentcarlkababoy@gmail.com>
2025-07-22 14:31:37 +0800

    Changes on firebase file

--- app/firebase.py
+++ app/firebase.py
@@ -1,6 +1,18 @@
+import os
+import json
 from firebase_admin import credentials, initialize_app, db
 
-cred = credentials.Certificate("C:/Users/kentc/OneDrive/Desktop/SMARTSERVE/SampleAPI/firebase_credentials.json")
+
+firebase_cred_json = os.environ.get("FIREBASE_CREDENTIALS")
+
+if not firebase_cred_json:
+    raise ValueError("Missing FIREBASE_CREDENTIALS environment variable")
+
+
+cred_dict = json.loads(firebase_cred_json)
+cred = credentials.Certificate(cred_dict)
+
+
 firebase_app = initialize_app(cred, {
     'databaseURL': 'https://smartserve-17598-default-rtdb.asia-southeast1.firebasedatabase.app/'
 })
