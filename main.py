4cfbd483912fdb833b470f66527072df63a7f9c4
Kent Carl A. Banzon <kentcarlkababoy@gmail.com>
2025-07-25 00:04:14 +0800

    adding CORS in main python

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from app.routes.Users import users_bp
 from app.routes.Detection import detection_bp
-
+from flask_cors import CORS
 def create_app():
     app = Flask(__name__)
-
+    CORS(app)
 
     @app.route('/')
     def index():
