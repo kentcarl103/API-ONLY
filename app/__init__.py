6ff0698ae0f647d15b16efb31fbbabf34f69baff
Kent Carl A. Banzon <kentcarlkababoy@gmail.com>
2025-07-24 23:36:37 +0800

    adding CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from .routes import register_blueprints
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
+
+
     register_blueprints(app)
     return app
